b6ae4d529a330a259a4e1d74ad0d8337162c96ca
Tom Burdick <thomas.burdick@gmail.com>
2017-09-21 12:55:15 -0500

    navigation works

--- CSNative.framework/Headers/CSNHeroView.h
+++ CSNative.framework/Headers/CSNHeroView.h
@@ -1,9 +1,10 @@
 @import UIKit;
+@import WebKit;
 #import "CSNComponentView.h"
 #import "CSNAd.h"
 
 //IB_DESIGNABLE
-@interface CSNHeroView : UIView <CSNComponentView, UIGestureRecognizerDelegate>
+@interface CSNHeroView : UIView <CSNComponentView, UIGestureRecognizerDelegate, WKNavigationDelegate>
 @property (readonly, nonnull) CSNAd *ad;
 @property (readonly) uint64_t componentID;
 @property BOOL interactable;
